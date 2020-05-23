c76d3ac030d990c354b7fde622a7aedc779275cd
stck-shrimpy <capsulecho_0216@yahoo.co.jp>
2020-05-24 02:08:56 +0900

    make migrations

---
+++ database/migrations/2020_05_23_165431_add_user_id_to_reviews.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToReviews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->foreignId('user_id')->after('id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropForeign('reviews_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+    }
+}
