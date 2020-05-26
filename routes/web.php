ae7db4b4e5ef4d186544ec94a8719a1fca38bb50
stck-shrimpy <capsulecho_0216@yahoo.co.jp>
2020-05-26 21:48:32 +0900

    create a singletone controller

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/available', 'AvailableDate');
 Route::resource('reviews', 'ReviewController');
+Route::resource('reservations', 'ReservationController');
